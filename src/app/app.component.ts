34421099a7f3e95a6e7576ec27740a5b08de7e22
Brigitte9219 <mbpadilla4@misena.edu.co>
2023-11-30 09:38:14 -0500

    Actualizando y probando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';//Define componentes en Angular
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';//Muestra las vistas asociadas a las rutas en la aplicación
+import { PersonasComponent } from './personas/personas.component';
 
 @Component({
   selector: 'app-root',//Nombre del componente
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PersonasComponent],//Importa lo que necesitamos
   templateUrl: './app.component.html',//Define el archivo html
   styleUrl: './app.component.css'//Define el archivo de estilos basado en el html
 })
